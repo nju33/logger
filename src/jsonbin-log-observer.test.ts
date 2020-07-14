6b3c67617cd8d610735b272f8e1c66d2c2e6a62b
nju33 <nju33.ki@gmail.com>
2020-07-14 13:20:50 +0900

    fix: fix jsonbin observer

--- src/jsonbin-log-observer.test.ts
+++ src/jsonbin-log-observer.test.ts
@@ -7,15 +7,20 @@ import {
 describe('JsonbiinLogObserver', () => {
   jest.useFakeTimers()
 
+  const metadata = { id: 'foo' }
+  let prevBin: O.Option<Record<string, any>>
   let bin: Record<string, any>
   let fetch: jest.Mock
   let observer: TraitJsonbinLogObserver
   let onChange: jest.Mock
 
   beforeEach(() => {
+    prevBin = O.none
     bin = {
-      metadata: { id: 'foo' },
-      record: { history: [] }
+      metadata,
+      record: {
+        history: []
+      }
     }
     fetch = jest.fn().mockResolvedValue({
       ok: true,
@@ -30,14 +35,56 @@ describe('JsonbiinLogObserver', () => {
     // eslint-disable-next-line no-void
     void observer.observe('key', 2000, { secretKey: 'secretKey' })
 
-    jest.advanceTimersByTime(5000)
-
-    setImmediate(() => {
-      expect(onChange).toHaveBeenCalledWith({
-        binId: 'key',
-        prev: O.none,
-        current: bin
+    jest.advanceTimersByTime(2000)
+    await new Promise((resolve) => {
+      setImmediate(() => {
+        expect(onChange).toHaveBeenNthCalledWith(1, {
+          binId: 'key',
+          prev: prevBin,
+          current: bin
+        })
       })
+
+      prevBin = O.some(bin)
+      bin = {
+        metadata,
+        record: {
+          history: [{ type: 'info', message: 'foo' }]
+        }
+      }
+
+      resolve()
+    })
+
+    jest.advanceTimersByTime(2000)
+    await new Promise((resolve) => {
+      setImmediate(() => {
+        expect(onChange).toHaveBeenNthCalledWith(2, {
+          binId: 'key',
+          prev: prevBin,
+          current: bin
+        })
+      })
+
+      prevBin = O.some(bin)
+      bin = {
+        metadata,
+        record: {
+          history: [{ type: 'info', message: 'foo' }]
+        }
+      }
+
+      resolve()
+    })
+
+    observer.unobserve('foo')
+    onChange.mockClear()
+    await new Promise((resolve) => {
+      setImmediate(() => {
+        expect(onChange).not.toHaveBeenCalled()
+      })
+
+      resolve()
     })
   })
 })
